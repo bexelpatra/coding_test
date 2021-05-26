aabce3d5fadf1a02a2b39e10f9a6cff13d883166
a45hvn <sobfy01@gmail.com>
2021-05-27 04:19:05 +0900

    stack and que 4

--- src/CodingTest.java
+++ src/CodingTest.java
@@ -1,6 +1,7 @@
 import com.test.practice.hash.Hash_1;
 import com.test.practice.stack_queue.StackQueue_1;
 import com.test.practice.stack_queue.StackQueue_2;
+import com.test.practice.stack_queue.StackQueue_4;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
@@ -8,10 +9,11 @@ import java.util.Deque;
 
 public class CodingTest {
     public static void main(String[] args) {
-        int[] p = new int[]{1, 1, 9, 1, 1, 1};
+        int[] p = new int[]{11, 13, 14, 13, 1, 3};
+//        int[] p = new int[]{1, 1, 9, 1, 1, 1};
 //        int[] s = new int[]{1, 1, 1, 1, 1, 1};
         int loca = 0;
-        new StackQueue_2().solution2(p,loca);
+        new StackQueue_4().solution1(p);
 
     }
 }
--- src/com/test/practice/stack_queue/StackQueue_4.java
+++ src/com/test/practice/stack_queue/StackQueue_4.java
@@ -1,18 +1,42 @@
 package com.test.practice.stack_queue;
 
+import java.util.Stack;
+
 public class StackQueue_4 {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-
-        for (int i = 0; i < answer.length; i++) {
-            for (int j = i+1; j < answer.length; j++) {
-                if (prices[i] > prices[j]) {
-                    answer[i] = j-i;
-                    break;
+        for(int i=0;i<prices.length;i++){
+            int first = prices[i];
+            int ans = 0;
+            for(int j= i+1;j<prices.length;j++){
+                if(first<=prices[j]) {
+                    ans++;
                 }
-                if (j==answer.length-1) answer[i] = j-i;
             }
+
+            answer[i] = ans;
         }
         return answer;
+    }
+    public int[] solution1(int[] prices) {
+        Stack<Integer> beginIdxs = new Stack<>();
+        int i=0;
+        int[] terms = new int[prices.length];
+        // 시작점
+        beginIdxs.push(i);
+        for (i=1; i<prices.length; i++) {
+            // 인덱스가 비어있지 않고(while문을 벗어나기 위해서), 현재 시점과 스택에 들어간 시점의 값을 빼서 주가가 떨어지지 않은 시간을구한다.
+            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+                int beginIdx = beginIdxs.pop();
+                terms[beginIdx] = i - beginIdx;
+            }
+            beginIdxs.push(i);
+        }
+        while (!beginIdxs.empty()) {
+            int beginIdx = beginIdxs.pop();
+            terms[beginIdx] = i - beginIdx - 1;
+        }
+
+        return terms;
     }
 }
