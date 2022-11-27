a81a5d44dada2addda7640cfd3d6898de3cea96c
a45hvn <bexelpatra@gmail.com>
2022-11-27 16:40:50 +0900

    automata

--- src/com/test/backjun/Baek1025.java
+++ src/com/test/backjun/Baek1025.java
@@ -1,37 +1,66 @@
 package com.test.backjun;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Baek1025 {
     public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        char[][] graph = new char[n][m];
-
-        // graph 생성
-        for (int i = 0; i < n; i++) {
-            String temp = reader.readLine();
-            graph[i] = temp.toCharArray();
-        }
-
-        for (int i = 0; i < n; i++) { // 세로(열)
-            for (int j = 0; j < m; j++) { // 가로(행)
-
+        int a =975319753;
+        System.out.println(isSqare(a));
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(reader.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int m = Integer.parseInt(st.nextToken());
+//        char[][] graph = new char[n][m];
+//
+//        // graph 생성
+//        for (int i = 0; i < n; i++) {
+//            String temp = reader.readLine();
+//            graph[i] = temp.toCharArray();
+//        }
+//
+//        for (int i = 0; i < n; i++) { // 세로(열)
+//            for (int j = 0; j < m; j++) { // 가로(행)
+//                search(graph,i,j,n,m);
+//            }
+//        }
+//
+//        System.out.println(answer);
+    }
+    static int answer = -1;
+    public static void search(char[][] graph,int x, int y,int n, int m){
+        // 등차값을 구하기
+        for (int i = -n; i < n; i++) { // 세로(열)
+            for (int j = -m; j < m; j++) { // 가로(행)
+                if(i ==0 && j==0){
+                    continue;
+                }
+                int tempX = x;
+                int tempY = y;
+                int num =0;
+                while (0<=tempX&& 0<=tempY && tempX < n && tempY < m){
+                    num*=10;
+                    num+= (int)(graph[tempX][tempY]-'0');
+                    int root = (int)Math.sqrt(num);
+                    if(root*root == num){
+                        answer = Math.max(num,answer);
+                    }
+//                    if(isSqare(num)){
+//                    }
+                    tempX+=i;
+                    tempY+=j;
+                }
             }
         }
-
     }
-    public static int search(char[][] graph,int x, int y,int n, int m){
-        if(x>n-1 || y > m-1){
-            return 0;
+
+    public static boolean isSqare(int n){
+        long temp=0;
+        while(temp <n/2 || temp*temp <= n){
+            if (temp*temp == n) {
+                return true;
+            }
+            temp+=1;
         }
-        return 0;
+        return false;
     }
-
     public static int reverse(int n){
         int reverse=0;
         for(int i=10;n>0;){
