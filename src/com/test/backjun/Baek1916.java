2791dd25ca10973c9c5964e50307a31b321d5fa8
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2023-05-26 16:10:04 +0900

    Update Baek1916.java

--- src/com/test/backjun/Baek1916.java
+++ src/com/test/backjun/Baek1916.java
@@ -1,10 +1,10 @@
 package com.test.backjun;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -12,35 +12,100 @@ public class Baek1916 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		try2(reader);
+//		outOfMemory(reader);
+	}
+	private static void try2(BufferedReader reader) throws Exception{
+		int N = Integer.parseInt(reader.readLine());
+		int M = Integer.parseInt(reader.readLine());
+		Map<Integer,Map<Integer,Integer>> map = new HashMap<>();
+		PriorityQueue<Point> pq = new PriorityQueue<Point>();
+		StringTokenizer st = null;
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(reader.readLine());
+			int y = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			if(map.get(y) == null) {
+				map.put(y, new HashMap<>());
+			}
+			if(map.get(y).get(x)!=null) {
+				map.get(y).put(x, map.get(y).get(x)>p? p:map.get(y).get(x));
+			}else {
+				map.get(y).put(x, p);				
+			}
+		
+		}
+		st = new StringTokenizer(reader.readLine());
+		int from = Integer.parseInt(st.nextToken());
+		int to = Integer.parseInt(st.nextToken());
+		
+		for (Integer i : map.get(from).keySet()) {
+			pq.add(new Point(i, from, map.get(from).get(i)));
+		}
+		
+		while(!pq.isEmpty()) {
+			Point now = pq.poll();
+			if(map.get(now.x)== null) continue;
+			for (Integer key :  map.get(now.x).keySet()) {
+				if(map.get(now.x).get(key) + now.p< map.get(from).getOrDefault(key,Integer.MAX_VALUE) ) {
+					
+					map.get(from).put(key, map.get(now.x).get(key) + now.p);
+					pq.add(new Point(from,key, map.get(now.x).get(key) + now.p));
+				}
+			}
+		}
+		for (int ki: map.keySet()) {
+			for (int ki2: map.get(ki).keySet()) {
+				System.out.println(String.format("y : %d x : x : %d price : %d", ki,ki2,map.get(ki).get(ki2)));
+			}
+		}
+		System.out.println(map.get(from).get(to));
+	}
+	private static void outOfMemory(BufferedReader reader) throws IOException {
 		int N = Integer.parseInt(reader.readLine());
 		int M = Integer.parseInt(reader.readLine());
 //		List<List<Integer>> list = new ArrayList<>();
 		int[][] map = new int[N+1][N+1];
-		PriorityQueue<Point> pq = new PriorityQueue<Baek1916.Point>();
+		PriorityQueue<Point> pq = new PriorityQueue<Point>();
 		StringTokenizer st = null;
-		for (int i = 0; i < map.length; i++) {
-			Arrays.fill(map[i], -1);
-		}
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(reader.readLine());
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 			int p = Integer.parseInt(st.nextToken());
 			map[y][x] = p;
+//			map[x][y] = p;
 		}
 		st = new StringTokenizer(reader.readLine());
 		int from = Integer.parseInt(st.nextToken());
 		int to = Integer.parseInt(st.nextToken());
+		
 		for (int i = 1; i < map[from].length; i++) {
 			pq.add(new Point(from,i,map[from][i]));
 		}
+		int temp = 0;
 		while(!pq.isEmpty()) {
 			Point now = pq.poll();
-			for (int i = 1; i < map[now.y].length; i++) {
-//				if(map[now.y][i])
+			for (int dest = 1; dest < map[now.y].length; dest++) {
+				if(map[now.y][dest] != 0
+						&& map[now.y][dest] + now.p < map[from][dest]
+								|| map[from][dest]==0) {
+					temp = map[now.y][dest] + now.p;
+					map[from][dest] =  temp;
+//					map[dest][from] =  temp;
+					pq.add(new Point(from, dest, temp));
+				}
 				
 			}
 		}
+//		for (int i = 1; i < map.length; i++) {
+//			System.out.println(Arrays.toString(map[i]));
+//		}
+		System.out.println(map[from][to]);
 	}
 	static class Point implements Comparable<Point>{
 		public int x;
@@ -55,7 +120,7 @@ public class Baek1916 {
 		@Override
 		public int compareTo(Point o) {
 			// TODO Auto-generated method stub
-			return this.p - o.p;
+			return  this.p - o.p;
 		}
 		
 	}
