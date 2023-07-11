858a8b9138764b5607b6cd010094c402301d4a62
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2023-07-11 17:19:35 +0900

    test sorce updated

--- src/DockerTest.java
+++ src/DockerTest.java
@@ -1,9 +1,13 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.UUID;
 
 public class DockerTest {
 
 	public static void main(String[] args) {
 		// System.out.println("docker test start!!");
-		String x = "QR_CREATE_MANAGE_ID".toLowerCase();
+		String x = "MRHST_BHF_MANAGE_ID".toLowerCase();
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < x.length(); i++) {
@@ -15,5 +19,29 @@ public class DockerTest {
 			sb.append(x.charAt(i));
 		}
 		System.out.println(sb.toString());
+
+		LocalDate now = LocalDate.now();
+		LocalDate d = LocalDate.of(2023, 4, 8);
+
+		System.out.println(now.getDayOfYear() - d.getDayOfYear());
+		// String uuid = UUID.randomUUID().toString();
+		// String uuid = UUID.nameUUIDFromBytes("123".getBytes()).toString();
+		// System.out.println(uuid);
+		String test = "20230711161316039824";
+		// System.out.println(Long.parseLong(test));
+		String temp = "";
+		BigDecimal big = new BigDecimal(test);
+		System.out.println(big.remainder(BigDecimal.valueOf(1000000)).longValueExact());
+		System.out.println(big.longValueExact());
+		for (int i = 0; i < test.length(); i++) {
+			try {
+				temp = temp + 1;
+				System.out.println(Long.parseLong(temp));
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(i);
+			}
+		}
 	}
 }
