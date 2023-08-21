b700a69a4bb7ebb4175bf8b2cbb0bff49e66e93a
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2023-08-22 08:58:34 +0900

    Update Baek2565.java

--- src/com/test/backjun/Baek2565.java
+++ src/com/test/backjun/Baek2565.java
@@ -3,6 +3,9 @@ package com.test.backjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Baek2565 {
@@ -10,10 +13,23 @@ public class Baek2565 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
         StringTokenizer st = null;
+        int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(reader.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            arr[i] = new int[] { a, b };
+        }
+        Arrays.sort(arr, (o1, o2) -> {
+            return o1[0] - o2[0];
+        });
+        List<Integer>[] cross = new ArrayList[n];
+        Arrays.stream(cross).forEach(t -> t = new ArrayList<>());
+        for (int i = 0; i < arr.length - 1; i++) {
+            int[] now = arr[i];
+            for (int j = 0; j < now.length; j++) {
+
+            }
         }
     }
 }
