4b26d6aa39ff512c1d556bb3cb3ed0b7d72092fe
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2023-11-07 08:58:06 +0900

    Update FailCompilation.java

--- src/com/test/backjun/FailCompilation.java
+++ src/com/test/backjun/FailCompilation.java
@@ -16,9 +16,10 @@ public class FailCompilation {
                     .append(System.lineSeparator());
         }
         // 다시 풀고싶은 문제
-        String[] doOver = new String[] { "1753", "10837", "1238", "1021", "5397", "10819", "18352" };
+        String[] doOver = new String[] { "1753", "10837", "1238", "1021", "5397", "10819", "18352" ,"15683"};
         String[] doOverReason = new String[] { "", R.B.reason, R.A.reason, R.D.reason, R.E.reason, "문제 푸는 방법이 따로 있는듯",
-                "다익스트라 다시 한번 더" };
+                "다익스트라 다시 한번 더" ,"속도가 너무 느리고 코드가 간결하지 못하다"};
+        
 
         StringBuilder sb2 = new StringBuilder();
         for (int i = 0; i < doOver.length; i++) {
